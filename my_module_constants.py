3ea97c981c2a62252d464f62443f05cfa94c35f7
KseniaGalichenko <72312557+KseniaGalichenko@users.noreply.github.com>
2020-11-14 14:20:46 +0200

    Update my_module_constants.py

--- my_module_constants.py
+++ my_module_constants.py
@@ -1,6 +1,5 @@
 gravity_constant = 6.67 * 10 ** (-11)
 light_speed = 3 * 10 ** 8
-Plancks_constant = 6.6 * 10 ** (-34)
 Dirac_constant = 1.05 * 10 ** (-34)
 elementary_charge = 1.6 * 10 ** (-19)
 Boltzmann_constant = 1.4 * 10 ** (-23)
