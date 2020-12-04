ae566b630bf71bb8dbadc75b6088ba114b3cb8b6
KseniaGalichenko <72312557+KseniaGalichenko@users.noreply.github.com>
2020-12-04 22:55:10 +0200

    Delete laba_3_task_22.py

--- laba_3/laba_3_task_22.py
+++
@@ -1,8 +0,0 @@
-from numpy import pi
-from my_module_constants import Boltzmann_constant as k
-from my_module_constants import Euler_number as e
-from my_module_constants import Planck_constant as h
-T = 200
-E = 300
-N = (2 / pi ** 0.5) * (h / ((k * T) ** (1.5))) * e ** ((-E) / k * T) * E ** (T / 2)
-print(N)
