ca9787ad14ca59e2842b1920e8b2d0d1d2ff6fe6
KseniaGalichenko <72312557+KseniaGalichenko@users.noreply.github.com>
2020-12-19 13:49:04 +0200

    Update lab_6_task_4.py

--- lab_6_task_4.py
+++ lab_6_task_4.py
@@ -1,14 +1,32 @@
 import matplotlib.pyplot as plt
 import numpy as np 
-from math import pi, sin, cos
+from numpy import sqrt, sin, cos
 
-def charts(k=3,title='Archimed'):
-  fi = np.arange(0.01,8*pi,1)
-  r = k * fi ** 0.5
-  x = r * cos(fi)
-  y = r * sin(fi)
-  plt.plot(x,y,label = 'Archimed')
-  plt.title(title)
-  plt.legend()
+def charts(g=4,b=1,k=1):
+  '''g=1 (логарифмическая спираль),
+     g=2 (архимедова спираль),
+     g=3 (спираль "жезл"),
+     g=4 (роза)
+  '''
+  if g==1:
+    phi = np.arange(0, 8*np.pi, 0.01)
+    r = np.exp(b * phi)
+    
+  elif g==2:
+    phi = np.arange(0, 8*np.pi, 0.01)
+    r = k * phi
+
+  elif g==3:
+    phi = np.arange(0.01, 8*np.pi, 0.01)
+    r = k / np.sqrt(phi)
+
+  elif g==4:
+    phi = np.arange(0, 8*np.pi, 0.01)
+    r = np.sin(k * phi)
+
+  x = r * np.cos(phi)
+  y = r * np.sin(phi) 
+
+  plt.plot(x,y,label = 'my_chart')
   plt.show()
 charts()
